321ef4247bca5b75d98d172d77ec2d592cc96f8d
lin-laptop-test <todor.chupov@gmail.com>
2018-12-08 03:12:48 +0200

    add empty solution

---
+++ 01.DefineClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DefineClass
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
