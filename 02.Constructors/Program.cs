0fe1f3732edf8f9d2afc2483c76557db9ec3857a
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2018-12-15 02:12:09 +0200

    add second task 02.Constructors

---
+++ 02.Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Constructors
+{
+    class Program
+    {
+        private const string model = "Nokia 3310 (2017)";
+        private const string manufacturer = "Nokia";
+        private const string price = "60 EUR";
+        private const string owner = "Tosh";
+
+        public string Model { get; set; }
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
