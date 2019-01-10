eea52215571f3c170cc912d4800b1e5723feb1a6
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2019-01-11 01:56:36 +0200

    Problem 7. GSM test

--- testApp/GSMtest.cs
+++ testApp/GSMtest.cs
@@ -8,19 +8,22 @@ using System.Threading.Tasks;
 
 namespace testApp
 {
-    class TestApp
+    class GSMtest
     {
         static void Main(string[] args)
         {
+            //Define array of devices (GSMs)
+            var gsm = new GSM[2];
+            
             //First GSM
-            var gsm = new GSM("S 7 Edge", "Samsung", "500$", owner: "Frank", batterySpec: new Battery(), displaySpec: new Display());
-            Console.WriteLine("First GSM\n" + gsm.ToString());
+            gsm[0] = new GSM("S 7 Edge", "Samsung", "500$", owner: "Frank", batterySpec: new Battery(), displaySpec: new Display());
+            Console.WriteLine("First GSM\n" + gsm[0].ToString());
 
             //Second GSM
-            var gsm2 = new GSM("S 7 Edge", "Samsung", "500$", owner: "Frank", 
+            gsm[1] = new GSM("S 7 Edge", "Samsung", "500$", owner: "Frank", 
                                 batterySpec: new Battery { BatteryModel = "Samsung 3600mAh battery", HoursIdle = 96, HoursTalk = 27, BatteryType = BatteryType.LiIon }, 
                                 displaySpec: new Display());
-            Console.WriteLine("\nSecond GSM\n" + gsm2.ToString());
+            Console.WriteLine("\nSecond GSM\n" + gsm[1].ToString());
 
             //Third GSM
             Console.WriteLine("\n" + GSM.IPhone4S);
