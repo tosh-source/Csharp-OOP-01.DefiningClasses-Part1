d4f87004b82aa7928cfcc83852506a18a0c73ba4
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2019-01-14 00:25:55 +0200

    Problem 8. Calls - all done

--- HomeworkDefiningClasses/Call.cs
+++ HomeworkDefiningClasses/Call.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace HomeworkDefiningClasses
 {
-    class Call
+    public class Call
     {
+        //Fields
+        private DateTime date = DateTime.Now;
+        private DateTime time = DateTime.Now;
+        private List<string> dialledPhoneNumber = new List<string>();
+        private List<uint> duration = new List<uint>();  //duration in seconds
+
+        //Property
+        public string Date
+        {
+            get
+            {
+                return this.date.ToString("HH.mm:SS");
+            }
+        }
+
+        public string Time
+        {
+            get
+            {
+                return this.time.ToString("dd-MM-yyyy");
+            }
+        }
+
+        public List<string> DialledPhoneNumber
+        {
+            get
+            {
+                return this.dialledPhoneNumber;
+            }
+            set
+            {
+                this.dialledPhoneNumber = value;
+            }
+        }
+
+        public List<uint> Duration
+        {
+            get
+            {
+                return this.duration;
+            }
+            set
+            {
+                this.duration = value;
+            }
+        }
+
+        //Def. constructor
+        public Call()
+        {
+
+        }
     }
 }
